adf74e88289c2204a40cfd5e3ce1dd7dde84567b
Cids08 <morales.carljohn1@gmail.com>
2026-04-19 16:02:07 +0800

    force refresh laravel config

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://sanrouqueelementaryschool.vercel.app',
+        'http://localhost:3000',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

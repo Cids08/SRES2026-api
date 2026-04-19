8ac4ece7e86fd814603d73016a8e5b90c39d9cc7
Cids08 <morales.carljohn1@gmail.com>
2026-04-19 17:13:34 +0800

    Fix admin seeder to use admins table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Admin;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
+        Admin::updateOrCreate(
             ['email' => 'morales.carljohn1@gmail.com'],
             [
                 'name' => 'Carl John Morales',
-                'bio' => 'System Administrator',
-                'profile_photo' => null,
                 'password' => Hash::make('Admin22026'),
             ]
         );

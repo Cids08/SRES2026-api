df85d800899ddf756bcc871dfedefe221261040e
Cids08 <morales.carljohn1@gmail.com>
2026-04-19 15:49:57 +0800

    update cors settings

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:5173',
+        'https://sanrouqueelementaryschool.vercel.app',
         'http://localhost:3000',
     ],
     'allowed_origins_patterns' => [],
